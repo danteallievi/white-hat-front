e1b5db5a4c0c2e0570846d05b93b1ff52a646923
osu <joanosuna9@gmail.com>
2021-12-18 16:46:54 +0100

    Added CreatePostCard component

---
+++ src/components/CreatePostCard/CreatePostCard.js
@@ -0,0 +1,20 @@
+import { ReactComponent as IconCreate } from "../../assets/icono-create.svg";
+
+import "./CreatePostCard.scss";
+
+const CreatePostCard = ({ onClick }) => {
+  return (
+    <article className="post-card">
+      <button className="button-create">
+        <IconCreate
+          width="80"
+          height="80"
+          className="button-create__icon"
+          onClick={onClick}
+        />
+      </button>
+    </article>
+  );
+};
+
+export default CreatePostCard;
