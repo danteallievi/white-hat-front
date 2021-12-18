245dd7e7835a7a4f096817573313363489654f3d
Dante Allievi <danteallievi@gmail.com>
2021-12-18 14:47:57 +0100

    New reducer created

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import loggedUser from "./loggedUserReducer";
 import currentUser from "./currentUserReducer";
+import currentPost from "./currentPostReducer";
 
 export default combineReducers({
   loggedUser,
   currentUser,
+  currentPost,
 });
