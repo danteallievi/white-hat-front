e74441e829bf819f184b1de4f6ea3163e9ce42fd
Xavier Lasierra <xlp1998@gmail.com>
2021-12-18 13:10:14 +0100

    Add header ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,33 @@
 import LoginButton from "../LoginButton/LoginButton";
 import LogoutButton from "../LogoutButton/LogoutButton";
+import WhButton from "../WhButton/WhButton";
+
+import "./Header.styles.scss";
 
 const Header = ({ isAuthenticated }) => {
+  const handleHomeNavigation = () => {
+    return;
+  };
+
+  const handleMySpaceNavigation = () => {};
+
   return (
-    <header>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</header>
+    <header className="header">
+      <h1 className="web-title">
+        White <span className="web-title web-title--secondary">Hat</span>
+      </h1>
+      <div className="header__button-container">
+        <WhButton text="Home" onClick={handleHomeNavigation} />
+        {isAuthenticated ? (
+          <>
+            <WhButton text="Mi espacio" onClick={handleMySpaceNavigation} />
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
+    </header>
   );
 };
 
--- src/components/LogoutButton/LogoutButton.js
+++ src/components/LogoutButton/LogoutButton.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import useStores from "../../hooks/useStores";
 
+import { ReactComponent as LogoutIcon } from "../../assets/icono-logout.svg";
+import "./LogoutButton.styles.scss";
+
 const LogoutButton = () => {
   const { logout } = useAuth0();
   const { logoutUser } = useStores();
@@ -11,7 +14,7 @@ const LogoutButton = () => {
     logoutUser();
   };
 
-  return <button onClick={onClick}>Log Out</button>;
+  return <LogoutIcon className="logout-button" onClick={onClick} />;
 };
 
 export default LogoutButton;
