7ce22f8c3b513fa4c7efd9d9acf098333ff8f942
SandraQS <sanquesan@gmail.com>
2021-12-18 14:20:17 +0100

    Add elements at Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,22 @@
+import { ReactComponent as ImageAmbient } from "../../assets/image_ambient.svg";
+import PostCard from "../../components/PostCard/PostCard";
+
+import { mockPosts } from "../../mocks/posts";
+
 const Home = () => {
-  return <p>Home works</p>;
+  const { posts } = mockPosts;
+  return (
+    <>
+      <header>
+        <ImageAmbient />
+        <h1>Titulo 1</h1>
+      </header>
+
+      {posts.map((post) => (
+        <PostCard post={post} key={post.id} />
+      ))}
+    </>
+  );
 };
 
 export default Home;
